b4678cfb6247aab704f3e8cff2cf0eb76b78ed1a
Jaimit <jaimit.jp@gmail.com>
2022-09-12 22:18:57 +0530

    No bugs

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_foreground_plugin/flutter_foreground_plugin.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 import 'package:sleep_tracker/Authentication/Onboarding/intro_screens.dart';
 import 'package:sleep_tracker/Authentication/login_signup.dart';
@@ -10,39 +9,11 @@ import 'home/home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp().then((value) {
-    // Get.put();
-  });
-
+  await Firebase.initializeApp().then((value) {});
   runApp(MyApp());
-  startForegroundService();
-}
-
-void startForegroundService() async {
-  await FlutterForegroundPlugin.setServiceMethodInterval(seconds: 5);
-  await FlutterForegroundPlugin.setServiceMethod(globalForegroundService);
-  await FlutterForegroundPlugin.startForegroundService(
-    holdWakeLock: false,
-    onStarted: () {
-      print("Foreground on Started");
-    },
-    onStopped: () {
-      print("Foreground on Stopped");
-    },
-    title: "Flutter Foreground Service",
-    content: "This is Content",
-    iconName: "ic_stat_hot_tub",
-  );
-}
-
-void globalForegroundService() {
-  debugPrint("current datetime is ${DateTime.now()}");
 }
 
 class MyApp extends StatelessWidget {
-  // const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
