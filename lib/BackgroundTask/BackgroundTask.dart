57d0750ef49607e9ef7581d671ff99fcb51ca937
Jaimit <jaimit.jp@gmail.com>
2022-09-15 10:46:52 +0530

    update

--- lib/BackgroundTask/BackgroundTask.dart
+++ lib/BackgroundTask/BackgroundTask.dart
@@ -1,7 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sleep_tracker/NoiseTracker/NoiseMeter.dart';
+import 'package:sleep_tracker/controller/constants.dart';
+import 'package:workmanager/workmanager.dart';
 
 class BackgroundTask {
   //Declaring the method Channel
@@ -9,10 +12,36 @@ class BackgroundTask {
   var data;
   String uid = null;
   var firestore = FirebaseFirestore.instance;
-
+  var simpleTaskKey = "periodicTask";
   //Constructor
   BackgroundTask() {
+    Workmanager().initialize(
+      callbackDispatcher,
+      isInDebugMode: true,
+    );
     CallNativeAndReturnCallBackDart();
+    WorkmanagerPluginInitialized();
+  }
+
+  WorkmanagerPluginInitialized() async {
+    Workmanager().registerPeriodicTask(
+      "periodic-task-identifier",
+      "periodicTask",
+      frequency: Duration(minutes: 15),
+    );
+  }
+
+  void callbackDispatcher() {
+    Workmanager().executeTask((task, inputData) async {
+      switch (task) {
+        case "periodicTask":
+          print("Task Called 15 Hrs");
+          updateUserSound();
+          break;
+      }
+      updateUserSound();
+      return Future.value();
+    });
   }
 
   CallNativeAndReturnCallBackDart() {
@@ -25,15 +54,14 @@ class BackgroundTask {
     });
   }
 
-
-/// It will retrieve the user latest sound for [10] and update it to the Database after calculating the average
+  /// It will retrieve the user latest sound for [10] and update it to the Database after calculating the average
   updateUserSound() {
     RecorderUtil util = RecorderUtil();
     util.start();
     Future.delayed(Duration(seconds: 10), () {
       util.stop();
       double _db = util.getAverageSoundForInterval();
-      User user = FirebaseAuth.instance.currentUser;
+      User user = auth.currentUser;
       if (user != null) {
         uid = user.uid;
       } else
@@ -42,15 +70,19 @@ class BackgroundTask {
 
       //get the previous average decibels of the user from Database
       firestore.collection("users").doc(uid).get().then((value) {
-        var prev = value["db"];
+        var prev = value.data().containsKey("db");
       });
 
       //no db field in firestore as db
-      if (prev == null)
-        firestore.collection("users").doc(uid).update({"db": _db});
+      if (prev == false)
+        firestore.collection("users").doc(uid).update({
+          "db": FieldValue.arrayUnion([_db])
+        });
 
       //updte the latest average db of the user in the user details
-      firestore.collection("users").doc(uid).update({"db": (prev + _db) / 2});
+      firestore.collection("users").doc(uid).update({
+        "db": FieldValue.arrayUnion([_db])
+      });
     });
   }
 
